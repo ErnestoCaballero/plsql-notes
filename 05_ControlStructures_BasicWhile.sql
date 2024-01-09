b933f68326a96074925d53bde3d530a626fba72a
Ernesto Caballero Delgado <ernestca@amdocs.com>
2024-01-08 19:10:41 -0600

    add basic while example

---
+++ 05_ControlStructures_BasicWhile.sql
@@ -0,0 +1,16 @@
+DECLARE
+    v_counter NUMBER(2) := 1;
+BEGIN
+    WHILE v_counter <= 10 LOOP
+        dbms_output.put_line('My counter is: ' || v_counter);
+        v_counter := v_counter+1;
+        
+        --EXIT WHEN v_counter > 7; --It works as a break in java when a condition is meet
+        
+        --This would also stop the execution:
+        IF v_counter > 8 THEN
+            EXIT;
+        END IF;
+
+    END LOOP;
+END;

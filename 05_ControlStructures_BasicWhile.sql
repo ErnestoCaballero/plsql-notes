bd391b8a7b693a7c9cfb6b8c946e311c9084078d
Ernesto Caballero Delgado <ernestca@amdocs.com>
2024-01-08 23:23:19 -0600

    create infinite loop

--- 05_ControlStructures_BasicWhile.sql
+++ 05_ControlStructures_BasicWhile.sql
@@ -14,3 +14,20 @@ BEGIN
 
     END LOOP;
 END;
+
+
+--How to create an infinite loop
+DECLARE
+    v_counter NUMBER := 1;
+BEGIN
+    WHILE TRUE LOOP
+        dbms_output.put_line('My while counter is: ' || v_counter);
+        v_counter := v_counter+1;
+        
+        IF v_counter = 10 THEN
+            EXIT;
+        END IF;
+        
+    END LOOP;
+END;
+       
